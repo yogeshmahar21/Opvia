5c4605e69ef7784ba16d69f13696ee5a12343bb4
Shashank <varshneys495@gmail.com>
2025-08-15 00:37:49 +0530

    Add Config and DB connection

---
+++ backend/src/config/config.js
@@ -0,0 +1,10 @@
+import { config as conf } from "dotenv";
+
+conf();
+
+const _config = {
+    port: process.env.PORT,
+    databaseURI: process.env.MONGOOSE_CONNECTION_STRING,
+};
+
+export const config = Object.freeze(_config);
