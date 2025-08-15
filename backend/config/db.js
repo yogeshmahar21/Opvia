f2e2f37b724fbe91ee75b71df010fc9796b18aff
Kajal15Shaw <kajalshaw28b@gmail.com>
2025-08-15 13:43:04 +0530

    auth

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-export const connectDB = async () => {
-  mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected successfully'))
-    .catch(err => console.error(err));
-};
