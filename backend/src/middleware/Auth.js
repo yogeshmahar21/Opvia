fb10d9df4252942cb3f4effb8d5f25aa4f67faa6
Shashank <varshneys495@gmail.com>
2025-08-15 18:47:15 +0530

    Add Auth middleware

---
+++ backend/src/middleware/Auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import config from '../config/config.js'
+import createHttpError from "http-errors"
+
+const Auth = (req, res, next) => {
+    const { authHeader } = req.headers['authorization'];
+
+    if (typeof token == 'string') {
+        const Token = authHeader.split(' ')[1];
+
+        const decoded = jwt.verify(Token, config.secret );
+
+        req.userId = decoded.sub;
+
+    } else {
+        return next(createHttpError(401,'Token is expired'));
+    }
+
+    next();
+}
+
+export default Auth;
